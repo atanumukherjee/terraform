c798a8943d05bdc8e4693cd6a7eade79c70ce7b5
Atanu Mukherjee <atanu.mukherjee@cognizant.com>
2021-12-15 02:48:03 +0000

    Adding some more example for terraform concept

---
+++ terraform-demo/vpc_nat-demo7/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {
+
+  default = "eu-west-2"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    eu-west-2 = "ami-09a2a0f7d2db8baca"
+    eu-west-1 = ""
+    eu-west-3 = ""
+  }
+}
